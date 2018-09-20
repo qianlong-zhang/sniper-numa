52268c1647e5f89d1a53f81cd721d218cf4b2a88
Qianlong Zhang <qianlong2006@gmail.com>
2018-09-20 09:24:36 -0400

    still not finished

--- common/core/memory_subsystem/memory_manager_base.cc
+++ common/core/memory_subsystem/memory_manager_base.cc
@@ -60,6 +60,8 @@ MemoryManagerBase::NUMAgetCoreListWithMemoryControllers()
 {
     std::vector<core_id_t> core_list_before_numa =  getCoreListWithMemoryControllers();
 	core_id_t current_core_id = getCore()->getId();
+    UInt32 socket_counts = Sim()->getCfg()->getInt("perf_model/dram/num_controllers");
+    if (current_core_id < Config::getSingleton()->getTotalCores()/)
 }
 std::vector<core_id_t>
 MemoryManagerBase::getCoreListWithSocketAgents()
